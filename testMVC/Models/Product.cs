0107f7ade3fbd1ad2787f3c01b2858f11ccedeae
Thunnathorn <phantomzmc@gmail.com>
2019-02-26 10:46:42 +0700

    commit commit

---
+++ testMVC/Models/Product.cs
@@ -0,0 +1,11 @@
+namespace testMVC.Models
+{
+    public class Product
+    {
+        int id {get; set;}
+        string name {get; set;}
+        int price {get; set;}
+        string detail {get; set;}
+        int type_product {get; set;}
+    }
+}
